f184a5ebe4941f7e1e46b8fb364d76f75c98d95b
mohammed yaseen <yyaseen080@gmail.com>
2022-09-10 15:23:49 +0530

    url changes

--- src/constants/url.js
+++ src/constants/url.js
@@ -2,5 +2,5 @@
 // const ServiceURL = 'http://192.168.1.39/garage/src/API/';
 // live
 
-const ServiceURL = 'https://bf3c-116-68-111-110.in.ngrok.io/garage/src/API/';
+const ServiceURL = 'https://192.168.1.39/garage/src/API/';
 export default ServiceURL;
